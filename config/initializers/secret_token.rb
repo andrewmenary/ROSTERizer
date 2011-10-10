a3eb252096f3b8b6ca47bdd7c461974754dd8bb2
Andrew Menary <andrew.menary@gmail.com>
2011-10-10 12:36:01 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rosterizer::Application.config.secret_token = '69543c1aab03b6559a546e450c7402d707d46d40d8c9475c2ab4f57dbbf1c7fe5ccf6bdadc641cb5e609e5f1b7fbe50c33fb2abed34f0c5fe51841a9886a1da3'
